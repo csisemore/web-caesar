908b842bdfcb4b9ed41a506fb887f370b3970ab6
csisemore <43246653+csisemore@users.noreply.github.com>
2018-10-18 11:43:58 -0500

    final upload via webpage

--- main.py
+++ main.py
@@ -1,32 +1,54 @@
-from flask import Flask
+
+from flask import Flask, request
+from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
 form = """
-<!doctype html>
+<!DOCTYPE html>
 <html>
+	<head>
+		<style>
+			form {{
+				background-color: #eee;
+				padding: 20px;
+				margin: 0 auto;
+				width: 540px;
+				font: 16px sans-serif;
+				border-radius: 10px;
+			}}
+			textarea {{
+				margin: 10px 0;
+				width: 540px;
+				height: 120px;
+			}}
+		</style>
+	</head>
 	<body>
-        <form>
-        <label for="first-name">First Name</label>
-            <input id="first-name" type="text" name="first_name" />
-            <input type="submit" name="first_name" />
-        </form>
-    </body>
+      <!-- create your form here -->
+		<form method="POST">
+          <h3>Web Caesar</h3>
+				<label type="text" name="rot" value="0">Rotate by:</label>
+				<input type="text" name="rot" value="0" />
+				<textarea name="text">{0}</textarea>
+				<input type="submit" value="Submit Query">
+		</form>
+	</body>
 </html>
 """
 
-
 @app.route("/")
 def index():
-    return form
+    return form.format("")
 
-app.run()
+@app.route("/", methods=['POST'])
+def encrypt():
+	form_rot = int(request.form['rot'])
+	form_text = request.form['text']
+	encrypted = rotate_string(form_text,form_rot)
+	return form.format(encrypted)
 
-
-@app.route("/")
-def index():
-    return "Hello World"
 
 app.run()
